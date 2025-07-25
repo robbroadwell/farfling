354d1ea33e6ae8df8172f891d9bdc3e168a53450
Rob Broadwell <hello@robbroadwell.com>
2025-07-25 14:54:12 -0400

    wow, a page loads

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['your-image-hosting.com'],
+  },
 };
 
+
+module.exports = nextConfig;
 export default nextConfig;
